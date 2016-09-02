5c6be410fc65e8980d7d072b779cac743c934c12
TurbaVladimir <TurbaVladimir@yandex.ru>
2016-09-02 22:18:05 +0300

    Added sub groups support

--- api/groups.php
+++ api/groups.php
@@ -6,6 +6,11 @@
 *@copyright 2016 Vladimir Turba
 */
 
+if (file_exists('subgroups.php')) {
+	require_once "subgroups.php";
+} else {
+	require_once 'subgroups-default.php';
+}
 if (file_exists('settings.php')) {
 	require_once "settings.php";
 } else {
@@ -25,7 +30,15 @@ foreach ($nodes as $node) {
 
 if (file_exists("../custom")) {
 	$customGroups = scandir('../custom');
-	$groups = array_merge($groups,
-array_slice($customGroups, 2));
+	$groups = array_merge($groups, array_slice($customGroups, 2));
 }
+foreach ($groups as $key => $group) {
+	if (in_array($group, array_keys($subGroups))) {
+		foreach ($subGroups[$group] as $subGroup) {
+			$groups[] = "$group $subGroup";
+		}
+		unset($groups[$key]);
+	}
+}
+sort($groups);
 echo json_encode($groups, $jsonFlags);
