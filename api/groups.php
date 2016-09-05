d527ab6ead50e647990a8db4400a35643db710c2
TurbaVladimir <TurbaVladimir@yandex.ru>
2016-09-06 00:31:30 +0300

    Added course selection support

--- api/groups.php
+++ api/groups.php
@@ -17,21 +17,34 @@ if (file_exists('settings.php')) {
 	require_once 'settings-default.php';
 }
 
+$groups = [];
+if (isset($_REQUEST['course']) && $_REQUEST['course'] == 'custom') {
+	if (file_exists('../custom')) {
+		$customGroups = scandir('../custom');
+		$groups = array_slice($customGroups, 2);
+	}
+	sort($groups);
+	die(json_encode($groups, $jsonFlags));
+}
+
 $page = file_get_contents($url . $timeTable);
 $dom = new DOMDocument();
 @$dom->loadHTML($page);
 $xpath = new DOMXPath($dom);
 $div = $xpath->query('//div[h1[contains(text(),"Политехнический колледж")]]')->item(0);
-$nodes = $xpath->query('.//a[contains(@href,".xls")]', $div);
-$groups = [];
+$nodes = [];
+if (isset($_REQUEST['course']) && $_REQUEST['course']) {
+	$course = $xpath->query('.//td', $div)->item($_REQUEST['course'] - 1);
+	if ($course) {
+		$nodes = $xpath->query('.//a[contains(@href,".xls")]', $course);
+	}
+} else {
+	$nodes = $xpath->query('.//a[contains(@href,".xls")]', $div);
+}
 foreach ($nodes as $node) {
 	$groups[] = $node->nodeValue;
 }
 
-if (file_exists('../custom')) {
-	$customGroups = scandir('../custom');
-	$groups = array_merge($groups, array_slice($customGroups, 2));
-}
 foreach ($groups as $key => $group) {
 	if (in_array($group, array_keys($subGroups))) {
 		foreach ($subGroups[$group] as $subGroup) {
